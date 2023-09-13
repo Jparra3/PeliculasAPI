272ad39161b3551c98da464e856a33238fc72673
JhonatanItoSoft <jhonatan.parra@itolab.com.co>
2023-09-13 07:07:17 -0500

    Add CustomBaseController

--- PeliculasAPI/Controllers/CustomBaseController.cs
+++ PeliculasAPI/Controllers/CustomBaseController.cs
@@ -1,8 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
 
 namespace PeliculasAPI.Controllers
 {
     public class CustomBaseController: ControllerBase
     {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public CustomBaseController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        protected async Task<List<TDto>> Get<TEntidad, TDto>() where TEntidad : class
+        {
+            var entidades = await context.Set<TEntidad>().ToListAsync();
+            var dtos = mapper.Map<List<TDto>>(entidades);
+            return dtos;
+        }
+        protected async Task<ActionResult<TDto>> Get<TEntidad, TDto>(int id) where TEntidad : class, IId
+        {
+            var entidad = await context.Set<TEntidad>().AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            if (entidad == null)
+            {
+                return NotFound();
+            }
+
+            return mapper.Map<TDto>(entidad);
+        }
+
+        protected async Task<ActionResult> Post<TCreacion, TEntidad, TLectura>
+            (TCreacion creacionDto, string nombreRuta) where TEntidad : class, IId
+        {
+            var entidad = mapper.Map<TEntidad>(creacionDto);
+            context.Add(entidad);
+            await context.SaveChangesAsync();
+
+            var dtoLectura = mapper.Map<TLectura>(entidad);
+            return new CreatedAtRouteResult(nombreRuta, new { id = entidad.Id }, dtoLectura);
+        }
+
+        protected async Task<ActionResult> Put<TUpdate, TEntidad>
+            (int id, TUpdate updateDto) where TEntidad: class, IId
+        {
+            var entidad = mapper.Map<TEntidad>(updateDto);
+            entidad.Id = id;
+            context.Entry(entidad).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        protected async Task<ActionResult> Delete<TEntidad>(int id) where TEntidad : class, IId, new()
+        {
+            var existe = await context.Set<TEntidad>().AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new TEntidad() { Id = id });
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
 }
--- PeliculasAPI/Controllers/GenerosController.cs
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -8,75 +8,86 @@ namespace PeliculasAPI.Controllers
 {
     [ApiController]
     [Route("api/generos")]
-    public class GenerosController: ControllerBase
+    public class GenerosController: CustomBaseController
     {
-        private readonly ApplicationDbContext context;
-        private readonly IMapper mapper;
+        //Para controladores que realizan crud simples, una clase base (CustomBaseController)
+        //Es muy util porque nos permite reutilizar el código
+
+        //private readonly ApplicationDbContext context;
+        //private readonly IMapper mapper;
 
         public GenerosController(ApplicationDbContext context,
-            IMapper mapper)
+            IMapper mapper): base(context, mapper)
         {
-            this.context = context;
-            this.mapper = mapper;
+            //this.context = context;
+            //this.mapper = mapper;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<GeneroDto>>> Get()
         {
-            var entidades = await context.Generos.ToListAsync();
-            var dto =  mapper.Map<List<GeneroDto>>(entidades);
-            return dto;
+            //var entidades = await context.Generos.ToListAsync();
+            //var dto =  mapper.Map<List<GeneroDto>>(entidades);
+            //return dto;
+            return await Get<Genero,GeneroDto>();
         }
         
         [HttpGet("{id:int}", Name = "obtenerGenero")]
         public async Task<ActionResult<GeneroDto>> Get(int id)
         {
-            var entidad = await context.Generos.FirstOrDefaultAsync(x => x.Id == id);
+            //var entidad = await context.Generos.FirstOrDefaultAsync(x => x.Id == id);
 
-            if(entidad == null)
-            {
-                return NotFound();
-            }
+            //if(entidad == null)
+            //{
+            //    return NotFound();
+            //}
 
-            var dto = mapper.Map<GeneroDto>(entidad);
-            return dto;
+            //var dto = mapper.Map<GeneroDto>(entidad);
+            //return dto;
+            return await Get<Genero, GeneroDto>(id);
         }
         
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] GeneroCreateDto generoCreateDto)
         {
-            var entidad = mapper.Map<Genero>(generoCreateDto);
-            context.Add(entidad);
-            await context.SaveChangesAsync();
+            //var entidad = mapper.Map<Genero>(generoCreateDto);
+            //context.Add(entidad);
+            //await context.SaveChangesAsync();
 
-            var generoDto = mapper.Map<GeneroDto>(entidad);
-            return new CreatedAtRouteResult("obtenerGenero", new {id = generoDto.Id}, generoDto);
+            //var generoDto = mapper.Map<GeneroDto>(entidad);
+            //return new CreatedAtRouteResult("obtenerGenero", new {id = generoDto.Id}, generoDto);
+            return await Post<GeneroCreateDto, Genero, GeneroDto>(generoCreateDto, "obtenerGenero");
         }
        
         [HttpPut("{id:int}")]
         public async Task<ActionResult> Put(int id, [FromBody] GeneroUpdateDto generoUpdateDto)
         {
-            var entidad = mapper.Map<Genero>(generoUpdateDto);
-            entidad.Id = id;
-            context.Entry(entidad).State = EntityState.Modified;
-            await context.SaveChangesAsync();
-            return NoContent();
+            //var entidad = mapper.Map<Genero>(generoUpdateDto);
+            //entidad.Id = id;
+            //context.Entry(entidad).State = EntityState.Modified;
+            //await context.SaveChangesAsync();
+            //return NoContent();
+
+            return await Put<GeneroUpdateDto, Genero>(id, generoUpdateDto);
+
             //return new CreatedAtRouteResult("obtenerGenero", new { id = entidad.Id }, entidad);
         }
         
        [HttpDelete("{id:int}")]
        public async Task<ActionResult> Delete(int id)
        {
-           var existe = await context.Generos.AnyAsync(x => x.Id == id);
+           //var existe = await context.Generos.AnyAsync(x => x.Id == id);
 
-           if (!existe)
-           {
-               return NotFound();
-           }
+           //if (!existe)
+           //{
+           //    return NotFound();
+           //}
 
-           context.Remove(new Genero { Id = id });
-           await context.SaveChangesAsync();
-           return NoContent();
+           //context.Remove(new Genero { Id = id });
+           //await context.SaveChangesAsync();
+           //return NoContent();
+
+            return await Delete<Genero>(id);
        }
 
     }
